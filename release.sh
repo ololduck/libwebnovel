f0f960f271f978a2cbdda5563f3e864eb072a849
Paul Ad Vitam <paul@vit.am>
2024-09-05 19:16:46 +0200

    feat: add smart-release & git push to release script

--- release.sh
+++ release.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
+# TODO: add git branch check
 cargo test
 cargo clippy -r --all-targets --locked
 cargo semver-checks check-release --all-features
-cargo smart-release --execute
+cargo smart-release --execute --update-crate-index
+git push
