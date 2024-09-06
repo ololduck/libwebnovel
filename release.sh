cfc71d3d2e1b02573709b9f5b8e18f3ac8c6c137
Paul Ad Vitam <paul@vit.am>
2024-09-06 15:39:55 +0200

    fix(release): commit the readme after being generated by `cargo readme` in the release process

--- release.sh
+++ release.sh
@@ -8,6 +8,8 @@ cargo test --all-features
 cargo doc --no-deps -r --all-features
 cargo clippy -r --all-targets --all-features --locked
 cargo semver-checks check-release --all-features
-cargo readme >README.md && git add README.md
+cargo readme >README.md &&
+  git add README.md &&
+  git commit -m "chore(docs): Update README.md"
 cargo smart-release --execute --update-crates-index
 git push
