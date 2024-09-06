3ff0b259721588d05fdb060223c588dd2e106d2e
Paul Ad Vitam <paul@vit.am>
2024-09-06 15:45:56 +0200

    fix(release): fix typo in release script

--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ cargo doc --no-deps -r --all-features
 cargo clippy -r --all-targets --all-features --locked
 cargo semver-checks check-release --all-features
 cargo readme >README.md
-if !git diff --quiet README.md; then
+if ! git diff --quiet README.md; then
   echo "README.md has been changed; commiting."
   git add README.md && git commit -m "chore(docs): Update README.md"
 fi
