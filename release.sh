7c7610dc64c025bb1a04d17f3842f31141184b45
Paul Ad Vitam <paul@vit.am>
2024-09-06 15:43:51 +0200

    fix(release): add a check to commit the readme file only if it has changed

--- release.sh
+++ release.sh
@@ -8,8 +8,10 @@ cargo test --all-features
 cargo doc --no-deps -r --all-features
 cargo clippy -r --all-targets --all-features --locked
 cargo semver-checks check-release --all-features
-cargo readme >README.md &&
-  git add README.md &&
-  git commit -m "chore(docs): Update README.md"
+cargo readme >README.md
+if !git diff --quiet README.md; then
+  echo "README.md has been changed; commiting."
+  git add README.md && git commit -m "chore(docs): Update README.md"
+fi
 cargo smart-release --execute --update-crates-index
 git push
