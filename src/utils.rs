67ed3c0c2b2b2c83c7e9abe9019288ac16219224
Paul Ad Vitam <paul@vit.am>
2024-09-14 17:37:14 +0200

    feat: Use only one HTTP client
    Use LazyLock for HTTP client to improve initialization. This reduces the overhead of creating a new client for each request by reusing a single instance, thus hopefully opening only one TCP connection. Maybe that will convince webnovel hosts that we are not a threat.

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,13 @@
-use reqwest::blocking::ClientBuilder;
+use std::sync::LazyLock;
+
+use reqwest::blocking::{Client, ClientBuilder};
 use reqwest::IntoUrl;
 
 static USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
+static HTTP_CLIENT: LazyLock<Client> =
+    LazyLock::new(|| ClientBuilder::new().user_agent(USER_AGENT).build().unwrap());
 
 /// Just a custom get that sets a correct User-Agent & follows redirects
 pub(crate) fn get(url: impl IntoUrl) -> reqwest::Result<reqwest::blocking::Response> {
-    let client = ClientBuilder::new().user_agent(USER_AGENT).build()?;
-    client.get(url).send()
+    HTTP_CLIENT.get(url).send()
 }
